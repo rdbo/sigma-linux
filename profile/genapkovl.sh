3a63cb0dffb7d9cd0c57583f5be132029cfe05a5
rdbo <rdbodev@gmail.com>
2022-08-07 19:13:21 -0300

    added iwd to default services, added extra files, added group wheel to sudoers

--- profile/genapkovl.sh
+++ profile/genapkovl.sh
@@ -80,5 +80,6 @@ rc_add udev-settle sysinit
 rc_add udev-postmount default
 rc_add lightdm default
 rc_add local default
+rc_add iwd default
 
 tar -c -C "$tmp" etc | gzip -9n > $HOSTNAME.apkovl.tar.gz
