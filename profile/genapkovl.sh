2b82253cd299cc3a31b01823e1f5eacf77e2b25c
rdbo <rdbodev@gmail.com>
2023-02-06 22:11:54 -0300

    fixed topbar modules

--- profile/genapkovl.sh
+++ profile/genapkovl.sh
@@ -78,7 +78,6 @@ rc_add udev-trigger sysinit
 rc_add udev-settle sysinit
 rc_add udev-postmount default
 rc_add local default
-rc_add lightdm default
 rc_add iwd default
 
 tar -c -C "$tmp" etc | gzip -9n > $HOSTNAME.apkovl.tar.gz
