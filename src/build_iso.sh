27794ce9c7973facd3c37702ad4a3f07c49d30dd
rdbo <rdbodev@gmail.com>
2024-03-19 07:43:40 -0300

    moved kernel, initramfs and config to /boot

--- src/build_iso.sh
+++ src/build_iso.sh
@@ -3,9 +3,11 @@
 set -e
 
 mkdir -p "$ISO_DIR"
+mkdir -p "$ISO_DIR/boot"
+mkdir -p "$ISO_DIR/boot/grub"
 
 # Copy vmlinuz to ISO dir
-cp "$KERNEL_DIR/arch/$TARGET_ARCH/boot/bzImage" "$ISO_DIR/vmlinuz"
+cp "$KERNEL_DIR/arch/$TARGET_ARCH/boot/bzImage" "$ISO_DIR/boot/vmlinuz"
 
 # NOTE: The modules won't be installed because the kernel is configured
 #       so that the required modules are builtin
@@ -14,19 +16,18 @@ cp "$KERNEL_DIR/arch/$TARGET_ARCH/boot/bzImage" "$ISO_DIR/vmlinuz"
 # make INSTALL_MOD_PATH="$ISO_DIR" modules_install
 
 # Copy kernel config to ISO dir
-cp "$KERNEL_DIR/.config" "$ISO_DIR/config"
+cp "$KERNEL_DIR/.config" "$ISO_DIR/boot/config"
 
 # Copy initramfs to ISO dir
-cp "$INITRAMFS_PATH" "$ISO_DIR"
+cp "$INITRAMFS_PATH" "$ISO_DIR/boot/initramfs"
 
 # Copy GRUB config to ISO dir
-mkdir -p "$ISO_DIR/boot/grub"
 cat <<- EOF > "$ISO_DIR/boot/grub/grub.cfg"
 menuentry "$SYSNAME" {
 	echo "Loading vmlinuz..."
-	linux /vmlinuz
+	linux /boot/vmlinuz
 	echo "Loading initrd..."
-	initrd /initramfs
+	initrd /boot/initramfs
 }
 EOF
 
