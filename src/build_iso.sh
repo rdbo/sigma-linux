eb2eef0eb3fc83c2e8f2fabc633f4d6a9158db88
rdbo <rdbodev@gmail.com>
2024-03-21 17:23:27 -0300

    added background to grub

--- src/build_iso.sh
+++ src/build_iso.sh
@@ -24,8 +24,23 @@ cp "$INITRAMFS_PATH" "$ISO_DIR/boot/initramfs"
 # Copy squashfs to ISO dir
 cp "$SQUASHFS_PATH" "$ISO_DIR/rootfs.squashfs"
 
+# Copy GRUB wallpaper to /boot/grub
+cp "$SRC_DIR/grub_wallpaper.png" "$ISO_DIR/boot/grub/wallpaper.png"
+
 # Copy GRUB config to ISO dir
 cat <<- EOF > "$ISO_DIR/boot/grub/grub.cfg"
+set timeout=5
+
+insmod all_video
+loadfont /boot/grub/fonts/unicode.pf2
+
+insmod gfxterm
+set gfxmode=640x480
+terminal_output gfxterm
+
+insmod png
+background_image /boot/grub/wallpaper.png
+
 menuentry "$SYSNAME" {
 	echo "Loading vmlinuz..."
 	linux /boot/vmlinuz
