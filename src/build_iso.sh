f56d23ab4f3104fc30aa4dd8beb872fec3dd61e3
rdbo <rdbodev@gmail.com>
2024-03-22 05:51:56 -0300

    changed foreground vt color

--- src/build_iso.sh
+++ src/build_iso.sh
@@ -30,12 +30,20 @@ cp "$SRC_DIR/grub_wallpaper.png" "$ISO_DIR/boot/grub/wallpaper.png"
 # Copy GRUB config to ISO dir
 # NOTE: The vt.* kernel parameters are used for changing the virtual terminal (VT) theme
 # Sane defaults:
-# red: 1,222,57,255,0,118,44,204,128,255,0,255,0,255,0,255
-# green: 1,56,181,199,111,38,181,204,128,0,255,255,0,0,255,255
-# blue: 1,43,74,6,184,113,233,204,128,0,0,0,255,255,255,255
-vt_default_red="16,222,57,255,0,118,44,204,128,255,0,255,0,255,0,255"
-vt_default_green="26,56,181,199,111,38,181,204,128,0,255,255,0,0,255,255"
-vt_default_blue="32,43,74,6,184,113,233,204,128,0,0,0,255,255,255,255"
+# default_red: 0x00,0xaa,0x00,0xaa,0x00,0xaa,0x00,0xaa,0x55,0xff,0x55,0xff,0x55,0xff,0x55,0xff
+# default_grn: 0x00,0x00,0xaa,0x55,0x00,0x00,0xaa,0xaa,0x55,0x55,0xff,0xff,0x55,0x55,0xff,0xff
+# default_blu: 0x00,0x00,0x00,0x00,0xaa,0xaa,0xaa,0xaa,0x55,0x55,0x55,0x55,0xff,0xff,0xff,0xff
+# NOTE: The VT color table is not in order!
+#       From 'drivers/tty/vt/vt.c':
+#       ```
+#       const unsigned char color_table[] = { 0, 4, 2, 6, 1, 5, 3, 7,
+#				              8,12,10,14, 9,13,11,15 };
+#       ```
+#       This means that color "3" will actually be mapped to the RGB indices of color "6", for
+#       example.
+vt_default_red=0x10,0xaa,0x00,0xaa,0x00,0xaa,0x00,0xaa,0x55,0xff,0x55,0xff,0x55,0xff,0x55,0xff
+vt_default_grn=0x1a,0x00,0xaa,0x55,0x00,0x00,0xff,0xaa,0x55,0x55,0xff,0xff,0x55,0x55,0xff,0xff
+vt_default_blu=0x20,0x00,0x00,0x00,0xaa,0xaa,0xc8,0xaa,0x55,0x55,0x55,0x55,0xff,0xff,0xff,0xff
 cat <<- EOF > "$ISO_DIR/boot/grub/grub.cfg"
 set timeout=5
 
@@ -51,7 +59,7 @@ background_image /boot/grub/wallpaper.png
 
 menuentry "$SYSNAME" {
 	echo "Loading vmlinuz..."
-	linux /boot/vmlinuz vt.color=0x03 vt.default_red=$vt_default_red vt.default_grn=$vt_default_green vt.default_blu=$vt_default_blue
+	linux /boot/vmlinuz vt.color=0x03 vt.default_red=$vt_default_red vt.default_grn=$vt_default_grn vt.default_blu=$vt_default_blu
 	echo "Loading initrd..."
 	initrd /boot/initramfs
 }
