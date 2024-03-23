93e6adcc13ee4c22bb13d2a433a6cb87c3033517
rdbo <rdbodev@gmail.com>
2024-03-23 10:01:26 -0300

    added local river package (fixes lagging and other issues by using the master branch)

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,10 @@ FROM alpine:edge
 # Setup packages
 RUN printf "http://dl-cdn.alpinelinux.org/alpine/edge/main\nhttp://dl-cdn.alpinelinux.org/alpine/edge/community\nhttp://dl-cdn.alpinelinux.org/alpine/edge/testing\n" > /etc/apk/repositories
 RUN apk update
+# base packages
 RUN apk add gcc openssl-dev elfutils-dev xorriso grub grub-efi grub-bios git gpg make musl-dev flex bison linux-headers perl mtools squashfs-tools alpine-sdk doas netpbm netpbm-extras
+# river dependencies
+RUN apk add libevdev-dev libxkbcommon-dev pixman-dev scdoc wayland-dev wayland-protocols wlroots-dev zig
 
 # Create build user
 RUN adduser -D build -G abuild
--- src/build_repo.sh
+++ src/build_repo.sh
@@ -6,7 +6,8 @@ set -e
 
 mkdir -p "$REPO_DIR" "$APKTEMP_DIR"
 
-# Make temporary APK with compressed sources in the cache directory
+# sigma-conf
+## Make temporary APK with compressed sources in the cache directory
 mkdir -p "$APKTEMP_DIR/sigma-conf/"
 cp "$APK_DIR/sigma-conf/APKBUILD" "$APKTEMP_DIR/sigma-conf/"
 cd "$APK_DIR/sigma-conf"
@@ -14,5 +15,27 @@ tar -czf "$APKTEMP_DIR/sigma-conf/rootfs.tar.gz" rootfs
 cd "$APKTEMP_DIR/sigma-conf"
 abuild checksum
 
+# sigma-river
+mkdir -p "$APKTEMP_DIR/sigma-river/"
+cp "$APK_DIR/sigma-river/APKBUILD" "$APKTEMP_DIR/sigma-river/"
+cd "$APKTEMP_DIR/sigma-river"
+## clone repository and archive it
+if [ ! -d river ]; then
+	git clone --depth 1 https://codeberg.org/river/river river
+	cd river
+	git submodule update --init
+	cd ..
+else
+	cd river
+	git pull origin
+	cd ..
+fi
+tar -czf river.tar.gz river
+abuild checksum
+
 # Build repository with the local APKs
+cd "$APKTEMP_DIR/sigma-conf"
+abuild -rf -P "$REPO_DIR"
+
+cd "$APKTEMP_DIR/sigma-river"
 abuild -rf -P "$REPO_DIR"
