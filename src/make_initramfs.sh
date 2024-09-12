26fc493ad8c2f7df25bbbe5a5cfc390fd1c3b391
rdbo <rdbodev@gmail.com>
2024-09-12 18:49:53 -0300

    fix dracut initramfs generation

--- src/make_initramfs.sh
+++ src/make_initramfs.sh
@@ -2,4 +2,5 @@
 
 set -e
 
-dracut -r "$SQUASHFS_DIR" "$INITRAMFS_PATH"
+kernel_version="$(ls "$SQUASHFS_DIR/lib/modules" | head -n 1)"
+dracut --kver "$kernel_version" -r "$SQUASHFS_DIR" "$INITRAMFS_PATH"
