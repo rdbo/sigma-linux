fe7330871062ff4720b17a21608b9f12aabdb1cb
rdbo <rdbodev@gmail.com>
2024-09-12 21:00:17 -0300

    update

--- src/make_initramfs.sh
+++ src/make_initramfs.sh
@@ -3,4 +3,4 @@
 set -e
 
 kernel_version="$(ls "$SQUASHFS_DIR/lib/modules" | head -n 1)"
-dracut -c "$SQUASHFS_DIR/etc/dracut.conf" --force --kver "$kernel_version" -r "$SQUASHFS_DIR" "$INITRAMFS_PATH"
+dracut -c "$SRC_DIR/dracut.conf" --force --kver "$kernel_version" -r "$SQUASHFS_DIR" "$INITRAMFS_PATH"
