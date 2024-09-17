dd182f1c07392d53354704fce55719cdd6ed1c49
rdbo <rdbodev@gmail.com>
2024-09-17 06:29:39 -0300

    no longer fetch and build kernel by hand

--- src/fetch_kernel.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-set -e
-
-git clone --depth 1 -b "$KERNEL_BRANCH" "$KERNEL_GIT" "$KERNEL_DIR"
--- src/update_kernel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-cd "$KERNEL_DIR"
-git reset --hard HEAD
-git fetch origin "$KERNEL_BRANCH" --depth 1
-git checkout FETCH_HEAD
-cd ..
