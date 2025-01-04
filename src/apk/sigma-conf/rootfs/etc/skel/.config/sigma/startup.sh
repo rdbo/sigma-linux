8b4012d5211e425a480c633f7d5032975e615386
rdbo <rdbodev@gmail.com>
2025-01-04 20:39:55 +0000

    added startup file

---
+++ src/apk/sigma-conf/rootfs/etc/skel/.config/sigma/startup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+. sigma-common
+
+swaybg -m fill -i "$SIGMA_CONFIG_DIR/background" &
+/usr/libexec/pipewire-launcher # Starts pipewire and waits for it to initialize
+mako
+waybar
+easyeffects --gapplication-service
+sigma-vkbd --hidden
+
+exec <&-
--- src/apk/sigma-dwl/config.h
+++ src/apk/sigma-dwl/config.h
@@ -108,7 +108,7 @@ LIBINPUT_CONFIG_TAP_MAP_LMR -- 1/2/3 finger tap maps to left/middle/right
 static const enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TAP_MAP_LRM;
 
 /* If you want to use the windows key for MODKEY, use WLR_MODIFIER_LOGO */
-#define MODKEY WLR_MODIFIER_ALT
+#define MODKEY WLR_MODIFIER_LOGO
 
 #define TAGKEYS(KEY,SKEY,TAG) \
 	{ MODKEY,                    KEY,            view,            {.ui = 1 << TAG} }, \
