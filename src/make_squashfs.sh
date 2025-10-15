2874f218f8b8a4adc49b79ea03f5d8b91273d9eb
rdbo <rdbodev@gmail.com>
2025-10-15 22:01:14 +0000

    use bash as the default shell

--- src/make_squashfs.sh
+++ src/make_squashfs.sh
@@ -144,7 +144,7 @@ rc_add bluetooth default
 rc_add local default # used for start scripts
 
 # Setup regular user
-useradd -R "$SQUASHFS_DIR" -m -G wheel,audio,input,video,seat user
+useradd -R "$SQUASHFS_DIR" -s /bin/bash -m -G wheel,audio,input,video,seat user
 # passwd -R "$SQUASHFS_DIR" -d user
 chroot "$SQUASHFS_DIR" sh -c 'printf "user:pass" | chpasswd'
 
