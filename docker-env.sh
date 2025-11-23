321f472f04723d91cb2da80dd74871e246cb9502
rdbo <rdbodev@gmail.com>
2025-11-23 12:10:16 +0000

    added cross compilation environment for docker

--- docker-env.sh
+++ docker-env.sh
@@ -2,5 +2,31 @@
 
 set -e
 
+. src/config.sh >& /dev/null
+
+set_docker_arch() {
+	local osarch="$1"
+	local dockerarch=""
+	case "$osarch" in
+		x86_64) dockerarch="linux/amd64";;
+		aarch64) dockerarch="linux/aarch64";;
+		*) echo "[!] Failed to start docker for cross compilation target: unknown OS arch"; exit 1;;
+	esac
+
+	export DOCKER_DEFAULT_PLATFORM="$dockerarch"
+	echo "DOCKER_DEFAULT_PLATFORM=$DOCKER_DEFAULT_PLATFORM"
+}
+
+if [ "$DISTRO_TARGET_ARCH" != "$(uname -m)" ]; then
+	# Install binfmt
+	echo "[*] Setting up binfmt for cross compilation..."
+	doas modprobe binfmt_misc
+	doas mount -t binfmt_misc binfmt_misc /proc/sys/fs/binfmt_misc/
+	docker run --privileged --rm tonistiigi/binfmt --install all
+
+	echo "[*] Setting up cross target docker..."
+	set_docker_arch "$DISTRO_TARGET_ARCH"
+fi
+
 docker build -t sigma-linux .
 docker run --privileged -v "$(pwd):/app" --workdir /app -it sigma-linux sh
